96c6f3532e44271f4e6a3555e509bab89eab7021
VKPInfiniAI <prakash@infiniai.tech>
2023-02-01 23:49:33 +0530

    Chapter Completed

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -4,42 +4,43 @@ import mediapipe as mp
 
 
 class handDetector():
-    def __init__(self, mode=False, maxHands=2,modelC=1, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=2,modelComplexity=1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
-        self.modelC = modelC
+        self.modelComplexity = modelComplexity
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelC, self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelComplexity, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.result = self.hands.process(imgRGB)
+        self.results = self.hands.process(imgRGB)
         # print(result.multi_hand_landmarks)
 
-        if self.result.multi_hand_landmarks:
-            for handLand in self.result.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(img, handLand, self.mpHands.HAND_CONNECTIONS)
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
 
         return img
 
     def findPosition(self, img, handNo=0, draw=True):
-        PosList = []
-        if self.result.multi_hand_landmarks:
-            myHand = self.result.multi_hand_landmarks[handNo]
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
+                #print(id,lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                PosList.append([id, cx, cy])
-
+                #print(id,cx,cy)
+                lmList.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 10, (255, 255, 255), cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
-        return PosList
+        return lmList
 
 
 def main():
@@ -51,18 +52,18 @@ def main():
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
-        PosList = detector.findPosition(img)
+        lmList = detector.findPosition(img)
 
-        if len(PosList) != 0:
-            print(PosList[4])
+        if len(lmList) != 0:
+            print(lmList[4])
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
 
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_DUPLEX, 3, (255, 0, 0), 3)
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
-        cv2.imshow("Webcam", img)
+        cv2.imshow("Image", img)
         cv2.waitKey(1)
 
 
